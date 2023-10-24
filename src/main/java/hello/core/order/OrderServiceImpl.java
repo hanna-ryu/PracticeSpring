29782b5afd27792a8b1c89a0f17c62e359926a7f
Hanna <foureaf@gmail.com>
2023-10-24 12:45:37 +0900

    [섹션4 스프링 컨테이너와 스프링 빈]

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    //인터페이스에만 의존 / 구체적인 클래스는 전혀 모름 => DIP
+    private final MemberRepository memberRepository;
+    //인터페이스에만 의존 / 구체적인 클래스는 전혀 모름 => DIP
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
