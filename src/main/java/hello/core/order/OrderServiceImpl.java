81861cbaee1d2d1db3c4a95064fe9a95b0e7fee2
Hanna <foureaf@gmail.com>
2023-10-25 16:33:31 +0900

    [섹션 6 컴포넌트 스캔과 의존관계 자동 주입 시작하기]

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,7 +4,10 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     //인터페이스에만 의존 / 구체적인 클래스는 전혀 모름 => DIP
@@ -12,6 +15,7 @@ public class OrderServiceImpl implements OrderService{
     //인터페이스에만 의존 / 구체적인 클래스는 전혀 모름 => DIP
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
